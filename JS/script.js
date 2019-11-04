e2bf6323ec3ce09f26eb159ed62eb27a4beea078
Opblaasmaatje <joris.terwolbeek@outlook.com>
2019-11-04 15:36:40 +0100

    Hobby's user story af

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,17 @@
 function changeImage() {
     document.getElementById("WelkomFoto").src = "Images/Home_foto.jpeg"
+    document.getElementById("Inhoud").innerHTML = "Dit is mijn portfolio website. Hierin staat alles over mij, tot mijn hobby’s tot mijn vaardigheden en de projecten waar ik in deelgenomen heb. Neem gerust uw tijd en bekijk mijn website."
+    document.getElementById("Titel").innerHTML = "Welkom"
 }
 
 function changeImage2() {
     document.getElementById("WelkomFoto").src = "Images/Japan_Hobby.jpeg"
+    document.getElementById("Inhoud").innerHTML = "Ik ben een internationale student geweest in Japan, Hierdoor houd ik van alles wat met Japan te maken heeft en kan ik ook JLPT niveau 4 Japans."
+    document.getElementById("Titel").innerHTML = "Japan"
 }
 
 function changeImage3() {
     document.getElementById("WelkomFoto").src = "Images/Magic_Hobby.jpeg"
+    document.getElementById("Inhoud").innerHTML = "Een van mijn favourite hobby's is Magic The Gathering, een Kaartspel waar je speelt als een tovernaar en krachte spreuken gebruikt om je tegenstander te verslaan."
+    document.getElementById("Titel").innerHTML = "Magic The Gathering"
 }
