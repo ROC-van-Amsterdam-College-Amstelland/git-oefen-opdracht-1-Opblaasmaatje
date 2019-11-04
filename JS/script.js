815df742d8d4a75ccd858c95659e93d8c34b794c
Opblaasmaatje <joris.terwolbeek@outlook.com>
2019-11-04 14:04:29 +0100

    JS gebruikt om foto's te veranderen

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+function changeImage() {
+    document.getElementById("WelkomFoto").src = "Images/Home_foto.jpeg"
+}
+
+function changeImage2() {
+    document.getElementById("WelkomFoto").src = "Images/Japan_Hobby.jpeg"
+}
+
+function changeImage3() {
+    document.getElementById("WelkomFoto").src = "Images/Magic_Hobby.jpeg"
+}
